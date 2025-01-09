06b5a8639567d3620f6badae75b236933b7e3899
sdm-max <epposon@gmail.com>
2025-01-09 20:46:14 +0900

    3

--- esp597/src/main.cpp
+++ esp597/src/main.cpp
@@ -1,28 +1,22 @@
 #include <Arduino.h>
 
-//선언한다 15핀은 myled 선언  15핀을 사용 한다
+//15번핀에 연결된 LED를 1초간격으로 깜빡이게 하여라!
+//나의 LED가 15번핀에 연결되었는데 15라고 쓰면 뭔가 멋이없으니
+//myled라는 별명을 붙혀주겠다!
 #define myled 15
 
-
 void setup() {
-  
-//led 설정한 핀을 출력으로 설정할것
- pinMode(myled,OUTPUT);
-
+  //LED가 연결된 핀을 출력으로 설정할 것
+  pinMode(myled,OUTPUT);
 }
 
 void loop() {
- 
-//led 켠다 
-//1초쉰다
-//led 끈다
-//1쉰다
-digitalWrite(myled,HIGH);
-delay(1000);
-digitalWrite(myled,LOW);
-delay(1000);
-
-
-
-
+  //LED를 켠다
+  digitalWrite(myled,HIGH);
+  //1초쉰다
+  delay(1000);
+  //LED를 끈다
+  digitalWrite(myled,LOW);
+  //1초쉰다
+  delay(1000);
 }
